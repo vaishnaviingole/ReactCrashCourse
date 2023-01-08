9ea9b2e9cbdf3bbef499415ce0bd93827d31b8b6
vaishnaviingole <vaishnavingole54@gmail.com>
2023-01-08 14:55:12 +0530

    lEARNED ABOU STATE AND HOOKS

--- contactapp/src/components/AddContact.js
+++ contactapp/src/components/AddContact.js
@@ -2,18 +2,38 @@ import React from 'react';
 //class component
 //you need to use render method in class component
 class AddContact extends React.Component{
+  state={
+    name:"",
+    email:""
+  }
+
+  add=(e)=>
+  {
+    e.preventDefault();//to prevent page from refreshing;
+    if(this.state.name=="" || this.state.email===""){
+      alert("All feilds are mandatory!");
+      return;
+    }
+    this.props.addContactHandler(this.state);//to add tothe array in app.js
+    this.setState({name:"",email:""});//to reset after fillling once
+
+  }
   render(){
     return(
         <div className='ui main'>
             <h2>AddContact</h2>
-            <form className='ui form'>
+            <form className='ui form' onSubmit={this.add}>
                 <div className='field'>
                     <label for='name'>Name</label>
-                    <input type="text" name="name" placeholder='Name'></input>
+                    <input type="text" name="name" placeholder='Name' value={this.state.name} 
+                    onChange={(e)=>this.setState({name:e.target.value})}></input>
+                    
+                    {/* here in onchange e is event */}
 
                 </div>
                 <label for='email'>Email</label>
-                    <input type="email" name="email" placeholder='email'></input>
+                    <input type="email" name="email" placeholder='email' value={this.state.email}
+                    onChange={(e)=>this.setState({email:e.target.value})}></input>
                     <button className='ui button blue'>Add</button>
             </form>
         </div>
--- contactapp/src/components/App.js
+++ contactapp/src/components/App.js
@@ -1,29 +1,35 @@
 
 import './App.css';
-import React from "react";
+import React ,{useState}from "react";//rect hook:useState
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 function App() {
+  const [contacts,setContacts]=useState([]);//here setContact helps to set state of contact you should not directly change contact
   //Rendering a list
-  const contacts=[{
-    id:"1",
-    name:"dipesh",
-    email:"malvia@gmail.com",
-  },
-  {
-    id:"2",
-    name:"shivesh",
-    email:"sh@gmail.com",
-  }
-  //to pass this array to contact list we use props--->props pass data from parent to child
+  // const contacts=[{
+  //   id:"1",
+  //   name:"dipesh",
+  //   email:"malvia@gmail.com",
+  // },
+  // {
+  //   id:"2",
+  //   name:"shivesh",
+  //   email:"sh@gmail.com",
+  // }
+  // //to pass this array to contact list we use props--->props pass data from parent to child
   
-];
+//];
+const addContactHandler=(contact)=>{
+  console.log(contact);
+  setContacts([...contacts,contact]);//to add feilds in contact state
+}
   return (
     
    <div className='Ui container'>
     <Header></Header>
-    <AddContact/>
+    <AddContact addContactHandler={addContactHandler}/>
+    {/* function as a prop */}
     <ContactList contacts={contacts}></ContactList>
     {/* here contact is a property name and contact inside curly braces is array*/}
    </div>
